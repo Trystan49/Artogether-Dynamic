70560927e2e71a63e03d7f8cc759513f58900058
Trystan49 <creation_tristoune_1@outlook.fr>
2022-04-18 22:20:12 +0200

    Ajout éléments calendrier

--- public/JS/calendar.js
+++ public/JS/calendar.js
@@ -2,43 +2,65 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    locale: 'fr',
+    initialView: "dayGridMonth",
+    locale: "fr",
     weekNumbers: true,
+    navLinks: true, // can click day/week names to navigate views
+    businessHours: true, // display business hours
+    editable: true,
+    selectable: true,
     headerToolbar: {
       left: "prev,next today",
       center: "title",
-      right: "dayGridMonth,timeGridWeek,list",
+      right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
     },
     buttonText: {
-      today: 'Aujourd\'hui',
-      month: 'Mois',
-      week: 'Semaine',
-      list: 'Liste',
-  },
+      today: "Aujourd'hui",
+      month: "Mois",
+      week: "Semaine",
+      day: "Jour",
+      list: "Liste",
+    },
     initialDate: "2022-07-01",
     navLinks: true, // can click day/week names to navigate views
     businessHours: true, // display business hours
     events: [
       {
-        title: "Business Lunch",
-        start: "2020-09-03T13:00:00",
-        constraint: "businessHours",
+        title: "Présentation du site",
+        start: "2022-07-01",
+        color: "lightgreen",
       },
       {
-        title: "Meeting",
-        start: "2020-09-13T11:00:00",
-        constraint: "availableForMeeting", // defined below
-        color: "#257e4a",
+        title: "Ouverture du site",
+        start: "2022-08-31",
+        color: "green",
       },
       {
-        title: "Conference",
-        start: "2020-09-18",
-        end: "2020-09-20",
+        title: "Forum ouvert",
+        start: "2022-09-31",
+        color: "yellow",
       },
       {
-        title: "Party",
-        start: "2020-09-29T20:00:00",
+        title: "Maintenance du site",
+        start: "2022-10-13T11:00:00",
+        end: "2022-10-14T11:00:00",
+        color: "red",
+      },
+      {
+        title: "Evolution du site",
+        start: "2022-10-15",
+        color: "purple",
+      },
+      {
+        title: "Maintenance du site",
+        start: "2023-03-23T11:00:00",
+        end: "2023-03-24T11:00:00",
+        color: "red",
+      },
+      {
+        title: "Mise en place de la boutique",
+        start: "2023-03-25",
+        color: "blue",
       },
 
       // areas where "Meeting" must be dropped
