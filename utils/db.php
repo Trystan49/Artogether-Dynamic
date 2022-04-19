2f88a10fed8b750baf1d57058dab84fc7b546980
Trystan49 <creation_tristoune_1@outlook.fr>
2022-04-19 22:39:30 +0200

    Sécurisation BDD

--- utils/db.php
+++ utils/db.php
@@ -3,14 +3,14 @@
 $server = "localhost";
 $db = "artogether";
 $user = "admin";
-$passwd = "1234";
+$passwdBdd = "1234";
 // Fin de la déclaration des paramètres
 
 // Cette partie est générique à l'ensemble de vos projets utilisant une base de données.
 $dsn = "mysql:host=$server;dbname=$db";
-$pdo = new PDO($dsn, $user, $passwd);
+$pdo = new PDO($dsn, $user, $passwdBdd);
 try {
-  $conn = new PDO($dsn, $user, $passwd);
+  $conn = new PDO($dsn, $user, $passwdBdd);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
